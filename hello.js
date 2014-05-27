ac030b2dda72055b31c57127fdf525b5829dfb23
Gee Bee <greg.bunia@gmail.com>
2014-05-27 12:01:46 +0200

    Using ClosureBuilder to create one minified js file.

--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
+goog.provide('myproject.start');
+
 goog.require('goog.dom');
 
-function sayHi() {
+myproject.start = function () {
   var newHeader = goog.dom.createDom('h1', {'style': 'background_color: #EEE'},
     'Hello World');
   goog.dom.appendChild(document.body, newHeader);
