88053cd642b4e2e7def8777da93c008681822101
Gee Bee <greg.bunia@gmail.com>
2014-05-27 11:06:42 +0200

    Add click event listener to contentElement.
    On click replace content with editable text area.

--- notepad.js
+++ notepad.js
@@ -2,6 +2,7 @@ goog.provide('notepad.Note');
 
 goog.require('goog.dom');
 goog.require('goog.ui.Zippy');
+goog.require('goog.events');
 
 notepad.Note = function(title, content, noteContainer) {
   this.title = title;
@@ -13,12 +14,33 @@ notepad.Note.prototype.makeNoteDom = function() {
   this.headerElement = goog.dom.createDom('div',
     {'style': 'background-color: #EEE'}, this.title);
   this.contentElement = goog.dom.createDom('div', null, this.content);
+
+  this.editorElement = goog.dom.createDom('textarea');
+  var saveBtn= goog.dom.createDom('input', {type: 'button', value: 'Save'});
+  this.editorContainer = goog.dom.createDom('div', {style: 'display: none'},
+    this.editorElement, saveBtn);
+
   var newNote = goog.dom.createDom('div', null,
-    this.headerElement, this.contentElement);
+    this.headerElement, this.contentElement, this.editorContainer);
 
   goog.dom.appendChild(this.parent, newNote);
 
+  goog.events.listen(this.contentElement, goog.events.EventType.CLICK, this.openEditor);
+
   return new goog.ui.Zippy(this.headerElement, this.contentElement);
+};
+
+notepad.Note.prototype.openEditor = function(event) {
+  var elt = event.target;
+  var content = goog.dom.getTextContent(elt);
+
+  var editorContainer = goog.dom.getNextElementSibling(elt);
+  var editor = goog.dom.getFirstElementChild(editorContainer);
+
+  editor.innerHTML = content;
+
+  elt.style.display = 'none';
+  editorContainer.style.display = 'inline';
 };
 
 notepad.makeNotes = function(data, noteContainer) {
